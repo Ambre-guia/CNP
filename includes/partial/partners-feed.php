fa7e9ebc8a8423a383e5b5895f9d3fb60b954cd5
Ambre-guia <ambreguia77@gmail.com>
2024-04-12 11:34:02 +0200

    update feed

--- includes/partial/partners-feed.php
+++ includes/partial/partners-feed.php
@@ -24,11 +24,7 @@
                 
                 // Do something...
                 ?>
-        <span class="logo-item">
-            <a href="<?= $link;?>" target="<?= $target;?>">
-                <img src="<?= $image['url'];?>" alt="<?= $image['alt'];?>" class="item-icon" >
-            </a>
-        </span>
+        
         <?php
             endwhile;
         endif;
