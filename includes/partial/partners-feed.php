a52983d71ee9d2ab30a71ec01e4db31b38c15bd5
Ambre-guia <ambreguia77@gmail.com>
2024-04-12 11:38:03 +0200

    update feed

--- includes/partial/partners-feed.php
+++ includes/partial/partners-feed.php
@@ -7,24 +7,27 @@
             // Loop through rows.
             while( have_rows('image_repeater') ) : the_row();
                 // Load sub field value.
-                var_dump(get_sub_field('image_repeater_url'));
-                // $image = get_sub_field('image_repeater_img');
-                // $link = "#";
-                // if(get_sub_field('image_repeater_url'))
-                // $link = get_sub_field('image_repeater_url');
-                // $target = "";
+                $image = get_sub_field('image_repeater_img');
+                $link = "#";
+                if(get_sub_field('image_repeater_url'))
+                $link = get_sub_field('image_repeater_url');
+                $target = "";
 
-                // if($link){
-                //     $link = $link['url'];
-                //     $target = $link['target'];
+                if($link){
+                    $link = $link['url'];
+                    $target = $link['target'];
                   
-                // }
-                // else
-                // $link="#";
+                }
+                else
+                $link="#";
                 
                 // Do something...
                 ?>
-        
+        <span class="logo-item">
+            <a href="<?= $link;?>" target="<?= $target;?>">
+                <img src="<?= $image['url'];?>" alt="<?= $image['alt'];?>" class="item-icon" >
+            </a>
+        </span>
         <?php
             endwhile;
         endif;
