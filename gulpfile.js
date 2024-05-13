85619236223d895262d8c9a89c4595c92b812c72
Ambre-guia <ambreguia77@gmail.com>
2024-05-13 17:47:55 +0200

    gulpfile update

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,33 @@
 const gulp = require("gulp");
+const concat = require("gulp-concat");
 const uglify = require("gulp-uglify");
 const cleanCSS = require("gulp-clean-css");
 // const htmlmin = require('gulp-htmlmin');
 
 gulp.task("minify-css", function () {
-  return gulp
-    .src("css/*.css")
-    .pipe(cleanCSS({ compatibility: "ie8" }))
-    .pipe(gulp.dest("_site"));
+  return (
+    gulp
+      .src("css/*.css")
+      //.pipe(concat("app.css"))
+      .pipe(cleanCSS({ compatibility: "ie8" }))
+      .pipe(gulp.dest("assets/css"))
+  );
 });
 
 gulp.task("minify-js", function () {
-  return gulp.src("js/*.js").pipe(uglify()).pipe(gulp.dest("_site"));
+  return (
+    gulp
+      .src("js/*.js")
+      //.pipe(concat("script.js"))
+      .pipe(uglify())
+      .pipe(gulp.dest("assets/js"))
+  );
 });
 
 // gulp.task('minify-html', function() {
-//   return gulp.src('_site/**/*.html')
+//   return gulp.src('assets/**/*.html')
 //     .pipe(htmlmin({collapseWhitespace: true}))
-//     .pipe(gulp.dest('_site'));
+//     .pipe(gulp.dest('assets/'));
 // });
 
 gulp.task("default", gulp.series("minify-css", "minify-js"));
