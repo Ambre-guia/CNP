eba8ebbe0f3229144a7d95f7a745ec508d21c665
Ambre-guia <ambreguia77@gmail.com>
2024-11-04 14:52:26 +0100

    modified:   header.php

--- header.php
+++ header.php
@@ -41,15 +41,12 @@
                 'theme_location' => 'header-menu',
                 'container' => 'ul',
             )); ?>
-<<<<<<< HEAD
-=======
-            
->>>>>>> 60c8dace5ae215c43ec9ab21cb97d34013b4731f
             <button class="hamburger hamburger--emphatic" type="button">
                 <span class="hamburger-box">
                     <span class="hamburger-inner"></span>
                 </span>
             </button>
+
         </nav>
     </header>
     <main>
